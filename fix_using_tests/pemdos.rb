50ca72743281979bd64bdb210d264d8c7e76a095
Yehong Piao <piaoyehong1107@gmail.com>
2020-06-24 06:14:19 +0000

    Done.

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -4,11 +4,19 @@ def snake_it_up(string)
   if string[0] == "s"
     i=0
     while (i<=9)
-      string[i]="s"
-      string = string[i]+string
+      string = "s" + string
       i+=1
     end
+    string
   else
     string
   end
 end
+
+#def snake_it_up(string)
+ # if string[0] == "s"
+ #   10 * "s" + string
+#  else
+ #   string
+#  end
+#end
